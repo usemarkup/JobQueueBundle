75a5bb5bd8a7b8384f26a3c9925376b905722d5c
Calum Brodie <calum@usemarkup.com>
2014-02-20 17:16:11 +0000

    Added command to start multiple queues based on the current queue config

---
+++ StartConfiguredQueuesCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Markup\JobQueueBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StartConfiguredQueuesCommand extends ContainerAwareCommand
+{
+
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('markup:job_queue:queue:start_all')
+            ->setDescription('Starts a list of available job queues that have been configured, with one worker per queue, DO NOT USE IN PRODUCTION');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->getContainer()->get('kernel')->getEnvironment() !== 'dev') {
+            throw new \Exception('Only for use in development');
+        }
+
+        $queues = $this->getContainer()->get('jobby')->getQueues();
+
+        $i = 0;
+        foreach ($queues as $queue) {
+            $command = $this->getApplication()->find('bcc:resque:worker-start');
+
+            $arguments = array(
+                'command' => 'bcc:resque:worker-start',
+                'queues'    => $queue
+            );
+
+            $input = new ArrayInput($arguments);
+            $command->run($input, $output);
+            $i++;
+        }
+
+        $output->writeln(sprintf('<info>Started %s queues</info>', $i));
+    }
+}

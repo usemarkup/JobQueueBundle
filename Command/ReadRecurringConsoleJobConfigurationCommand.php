820b55162f9324f9206b1f02c6265e9a330b1259
Douglas Greenshields <douglas@usemarkup.com>
2015-01-28 17:23:39 +0000

    remove unused use statement

--- Command/ReadRecurringConsoleJobConfigurationCommand.php
+++ Command/ReadRecurringConsoleJobConfigurationCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Process\Process;
 
 /**
  * This command reads the recurring job configuration and displays a table showing information about scheduled jobs

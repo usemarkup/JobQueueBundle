87c13859354125507de529bf091b4ea018c88a19
Douglas Greenshields <douglas@usemarkup.com>
2017-12-22 09:44:16 +0000

    fix: add symfony 3 support to search job logs form type

---
+++ Util/LegacyFormHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Markup\JobQueueBundle\Util;
+
+final class LegacyFormHelper
+{
+    /**
+     * @var string[]
+     */
+    private static $map = [
+        'Symfony\Component\Form\Extension\Core\Type\TextType' => 'text',
+        'Symfony\Component\Form\Extension\Core\Type\DateTimeType' => 'datetime',
+        'Symfony\Component\Form\Extension\Core\Type\ChoiceType' => 'choice',
+        'Symfony\Component\Form\Extension\Core\Type\HiddenType' => 'hidden',
+        'Symfony\Component\Form\Extension\Core\Type\SubmitType' => 'submit',
+    ];
+
+    /**
+     * @param $class
+     *
+     * @return mixed
+     */
+    public static function getType($class)
+    {
+        if (!self::isLegacy()) {
+            return $class;
+        }
+        if (!isset(self::$map[$class])) {
+            throw new \InvalidArgumentException(sprintf('Form type with class "%s" can not be found. Please check for typos or add it to the map in LegacyFormHelper', $class));
+        }
+        return self::$map[$class];
+    }
+    /**
+     * @return bool
+     */
+    public static function isLegacy()
+    {
+        return !method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix');
+    }
+
+    /**
+     * LegacyFormHelper constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+}
